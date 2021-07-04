4bf31e246fbfbb6e6c4e73a9af09a14f463bf55b
agile_tiger <11nature98@gmail.com>
2021-07-04 11:32:46 +0300

    Add empty private files

---
+++ Server/RoleInitializeData.private.cs
@@ -0,0 +1,12 @@
+﻿using EyE.Shared.Enums;
+using EyE.Shared.ViewModels.Identity;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+
+namespace EyE.Server
+{
+    public static class RoleInitializerData
+    {
+        
+    }
+}
---
+++ Server/Services/EmailServiceData.private.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EyE.Server.Services
+{
+    public static class EmailServiceData
+    {
+        
+    }
+}
